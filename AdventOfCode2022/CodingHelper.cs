1bba0cc2e723111b64d1dc4de4584a3057b76fed
konstitheprogrammer <kanzlerkonstantin@gmail.com>
2022-12-08 17:15:42 +0100

    added day 8

--- AdventOfCode2022/CodingHelper.cs
+++ AdventOfCode2022/CodingHelper.cs
@@ -3,53 +3,63 @@ using System.IO.Compression;
 
 namespace AdventOfCode2022;
 
-public class InputReader {
-    public static InputReader CreateInputReader() {
+public class InputReader
+{
+    public static InputReader CreateInputReader()
+    {
         return new InputReader();
     }
 
     private List<Input> Inputs { get; set; } = new();
     private int _currentInputIndex = 0;
 
-    public InputReader ReadZipFile(string zipFile, string splitValue = " ") {
+    public InputReader ReadZipFile(string zipFile, string splitValue = " ")
+    {
         UnzipFile(zipFile);
         ReadFolder(zipFile.Replace(".zip", ""), splitValue);
         return this;
     }
 
-    public InputReader UnzipFile(string zipFile) {
+    public InputReader UnzipFile(string zipFile)
+    {
         ZipFile.ExtractToDirectory(GetCompletePath(zipFile), GetCompletePath(zipFile.Replace(".zip", "")), true);
         return this;
     }
 
     //liest folder mit allen dateien ein
-    public InputReader ReadFolder(string folderName) {
+    public InputReader ReadFolder(string folderName)
+    {
         ReadFolder(folderName, " ");
         return this;
     }
 
-    private InputReader ReadFolder(string folderName, string splitValue) {
-        foreach (string file in Directory.EnumerateFiles(GetCompletePath(folderName), "*.in")) {
+    private InputReader ReadFolder(string folderName, string splitValue)
+    {
+        foreach (string file in Directory.EnumerateFiles(GetCompletePath(folderName), "*.in"))
+        {
             ReadWholeFile(file, splitValue);
         }
 
         return this;
     }
 
-    public static string GetCompletePath(string fileName) {
+    public static string GetCompletePath(string fileName)
+    {
         if (fileName.Contains("../")) return fileName;
         return "../../../" + fileName;
     }
 
     //list nur ein file ein mit standart splitValue
-    public InputReader ReadWholeFile(string fileName) {
+    public InputReader ReadWholeFile(string fileName)
+    {
         ReadWholeFile(fileName, " ");
         return this;
     }
 
 
     //list einen file ein und splittet nach splitValue
-    public InputReader ReadWholeFile(string fileName, string splitValue) {
+    public InputReader ReadWholeFile(string fileName, string splitValue)
+    {
         var input = File.ReadAllLines(GetCompletePath(fileName)).Select(k => k.Split(splitValue)).SelectMany(k => k)
             .ToList();
         Inputs.Add(new Input(fileName, input));
@@ -57,15 +67,18 @@ public class InputReader {
     }
 
     //liest file mit standart split value
-    public InputReader ReadFilePerLine(string filename) {
+    public InputReader ReadFilePerLine(string filename)
+    {
         ReadFilePerLine(filename, " ");
         return this;
     }
 
     //liest file mit splitvalue
-    public InputReader ReadFilePerLine(string filename, string splitValue) {
+    public InputReader ReadFilePerLine(string filename, string splitValue)
+    {
         var input = File.ReadLines("../../../" + filename);
-        foreach (var s in input) {
+        foreach (var s in input)
+        {
             Inputs.Add(
                 new Input(filename, s.Split(splitValue, StringSplitOptions.RemoveEmptyEntries).ToList()));
         }
@@ -74,12 +87,15 @@ public class InputReader {
     }
 
     //wenn man ein file mit mehreren Trennern hat zb ein leerzeichen und ein komma
-    public InputReader ReadFilePerLine(string filename, List<string> splitValues) {
+    public InputReader ReadFilePerLine(string filename, List<string> splitValues)
+    {
         if (splitValues.Count == 0) throw new Exception("There has to be at least 1 Split Value");
 
         var input = File.ReadLines("../../../" + filename).ToList();
-        for (int i = 0; i < input.Count; i++) {
-            foreach (var splitValue in splitValues) {
+        for (int i = 0; i < input.Count; i++)
+        {
+            foreach (var splitValue in splitValues)
+            {
                 input[i] = input[i].Replace(splitValue, splitValues[0]);
             }
 
@@ -92,13 +108,16 @@ public class InputReader {
 
 
     //bekommst alle inputs also alle files
-    public List<Input> GetInputs() {
+    public List<Input> GetInputs()
+    {
         return Inputs;
     }
 
     //naechstes file sofern es eins gibt
-    public Input? GetNextInput() {
-        if (_currentInputIndex >= Inputs.Count) {
+    public Input? GetNextInput()
+    {
+        if (_currentInputIndex >= Inputs.Count)
+        {
             return null;
         }
 
@@ -106,8 +125,10 @@ public class InputReader {
     }
 
     //bekommst input mit index
-    public Input? GetInputAt(int index) {
-        if (index >= Inputs.Count) {
+    public Input? GetInputAt(int index)
+    {
+        if (index >= Inputs.Count)
+        {
             return null;
         }
 
@@ -115,7 +136,8 @@ public class InputReader {
     }
 
     //speichert den consolen output in eine datei
-    public void InitOutputRedirection() {
+    public void InitOutputRedirection()
+    {
         FileStream filestream = new FileStream("../../../out.txt", FileMode.Create);
         var streamwriter = new StreamWriter(filestream);
         streamwriter.AutoFlush = true;
@@ -124,13 +146,15 @@ public class InputReader {
     }
 }
 
-public class Input {
+public class Input
+{
     public List<string> Inputs;
     public int Index;
     public string FileName { get; set; }
 
     //speichert alle console.writeline outputs in ein file
-    public void SetOutput() {
+    public void SetOutput()
+    {
         FileStream filestream = new FileStream(
             GetOutputPath("outputs/" + FileName),
             FileMode.Create);
@@ -139,13 +163,15 @@ public class Input {
         Console.SetOut(streamwriter);
     }
 
-    public Input(string fileName, List<string> inputs) {
+    public Input(string fileName, List<string> inputs)
+    {
         FileName = fileName;
         Inputs = inputs;
     }
 
 
-    private string GetOutputPath(string fileName) {
+    private string GetOutputPath(string fileName)
+    {
         var path = fileName.Replace(".in", "")
             .Split(new string[] { "/", "\\" }, StringSplitOptions.RemoveEmptyEntries);
         path[^2] += "Output";
@@ -154,36 +180,43 @@ public class Input {
     }
 
     //gibt das naechste fragment aus
-    public string Read() {
+    public string Read()
+    {
         return Inputs[Index++];
     }
 
 
-    public int ReadInt() {
+    public int ReadInt()
+    {
         return int.Parse(Read());
     }
 
-    public long ReadLong() {
+    public long ReadLong()
+    {
         return long.Parse(Read());
     }
 
-    public double ReadDouble() {
+    public double ReadDouble()
+    {
         return double.Parse(Read());
     }
 
-    public bool ReadBool() {
+    public bool ReadBool()
+    {
         return bool.Parse(Read());
     }
 
     //gibt das naechste fragment aus (mit anzahl an naechsten fragmenten)
-    public List<string> Read(int amount) {
+    public List<string> Read(int amount)
+    {
         var x = Inputs.GetRange(Index, amount);
         Index += amount;
         return x;
     }
 
     //setzt den index des fragment lesers
-    public void SetIndex(int index) {
+    public void SetIndex(int index)
+    {
         Index = index;
     }
 
@@ -195,34 +228,49 @@ public class Input {
     public bool HasEnded() => Index >= Inputs.Count;
 }
 
-public static class Extension {
-    public static T Pop<T>(this List<T> list) {
+public static class Extension
+{
+    public static T Pop<T>(this List<T> list)
+    {
         var item = list[0];
         list.RemoveAt(0);
         return item;
     }
 
-    public static T Pop<T>(this List<T> list, int index) {
+    public static T Pop<T>(this List<T> list, int index)
+    {
         var item = list[index];
         list.RemoveAt(index);
         return item;
     }
 
+    public static List<Node> ToNeighbors(this IEnumerable<IEnumerable<Node>> enumerable)
+        => enumerable.Select(k => k.ToArray()).ToArray().ToNeighbors();
 
-    public static List<Node> ToNeighbors<T>(this Node[][] array) {
+    public static List<Node> ToNeighborsFull(this IEnumerable<IEnumerable<Node>> enumerable)
+        => enumerable.Select(k => k.ToArray()).ToArray().ToNeighborsFull();
+
+    public static List<T> ToType<T>(this IEnumerable<Node> enumerable) where T : Node =>
+        enumerable.Select(k => k as T).ToList();
+
+
+    public static List<Node> ToNeighbors(this Node[][] array)
+    {
         var nodes = new List<Node>();
 
-        for (int y = 0; y < array.Length; y++) {
-            for (int x = 0; x < array[y].Length; x++) {
+        for (int y = 0; y < array.Length; y++)
+        {
+            for (int x = 0; x < array[y].Length; x++)
+            {
                 var left = (x - 1 >= 0);
                 var right = (x + 1 < array[y].Length);
                 var top = (y - 1 >= 0);
                 var bottom = (y + 1 < array.Length);
 
-                if (left) array[y][x].Neighbors.Add(array[y][x - 1]);
-                if (right) array[y][x].Neighbors.Add(array[y][x + 1]);
-                if (top) array[y][x].Neighbors.Add(array[y - 1][x]);
-                if (bottom) array[y][x].Neighbors.Add(array[y + 1][x]);
+                if (left) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y][x - 1], EDirection.W));
+                if (right) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y][x + 1], EDirection.E));
+                if (top) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y - 1][x], EDirection.N));
+                if (bottom) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y + 1][x], EDirection.S));
 
                 nodes.Add(array[y][x]);
             }
@@ -231,24 +279,31 @@ public static class Extension {
         return nodes;
     }
 
-    public static List<Node> ToNeighborsFull(this Node[][] array) {
+    public static List<Node> ToNeighborsFull(this Node[][] array)
+    {
         var nodes = new List<Node>();
 
-        for (int y = 0; y < array.Length; y++) {
-            for (int x = 0; x < array[y].Length; x++) {
+        for (int y = 0; y < array.Length; y++)
+        {
+            for (int x = 0; x < array[y].Length; x++)
+            {
                 var left = (x - 1 >= 0);
                 var right = (x + 1 < array[y].Length);
                 var top = (y - 1 >= 0);
                 var bottom = (y + 1 < array.Length);
 
-                if (left) array[y][x].Neighbors.Add(array[y][x - 1]);
-                if (right) array[y][x].Neighbors.Add(array[y][x + 1]);
-                if (top) array[y][x].Neighbors.Add(array[y - 1][x]);
-                if (bottom) array[y][x].Neighbors.Add(array[y + 1][x]);
-                if (left && top) array[y][x].Neighbors.Add(array[y - 1][x - 1]);
-                if (right && top) array[y][x].Neighbors.Add(array[y - 1][x + 1]);
-                if (left && bottom) array[y][x].Neighbors.Add(array[y + 1][x - 1]);
-                if (right && bottom) array[y][x].Neighbors.Add(array[y + 1][x + 1]);
+                if (left) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y][x - 1], EDirection.W));
+                if (right) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y][x + 1], EDirection.E));
+                if (top) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y - 1][x], EDirection.N));
+                if (bottom) array[y][x].Neighbors.Add(new Connection(array[y][x], array[y + 1][x], EDirection.S));
+                if (bottom && right)
+                    array[y][x].Neighbors.Add(new Connection(array[y][x], array[y + 1][x + 1], EDirection.SE));
+                if (bottom && left)
+                    array[y][x].Neighbors.Add(new Connection(array[y][x], array[y + 1][x - 1], EDirection.SW));
+                if (top && right)
+                    array[y][x].Neighbors.Add(new Connection(array[y][x], array[y - 1][x + 1], EDirection.NE));
+                if (top && left)
+                    array[y][x].Neighbors.Add(new Connection(array[y][x], array[y - 1][x - 1], EDirection.NW));
 
                 nodes.Add(array[y][x]);
             }
@@ -257,19 +312,23 @@ public static class Extension {
         return nodes;
     }
 
-    public static int ToInt32(this string s) {
+    public static int ToInt32(this string s)
+    {
         return Convert.ToInt32(s);
     }
 
-    public static long ToInt64(this string s) {
+    public static long ToInt64(this string s)
+    {
         return Convert.ToInt64(s);
     }
 
-    public static bool ToBool(this string s) {
+    public static bool ToBool(this string s)
+    {
         return Convert.ToBoolean(s);
     }
 
-    public static List<T> AddReturn<T>(this List<T> list, T element) {
+    public static List<T> AddReturn<T>(this List<T> list, T element)
+    {
         list.Add(element);
         return list;
     }
@@ -280,34 +339,92 @@ public static class Extension {
         list.Select(k => Convert.ToString(k)).Aggregate((a, b) => a + "," + b)!;
 }
 
-public class Node {
-    public List<Node> Neighbors { get; set; } = new List<Node>();
+public class Node
+{
+    public List<Connection> Neighbors { get; set; } = new List<Connection>();
+
+    public Connection? GetNeighbor(EDirection direction) => Neighbors.Find(k => k.Direction == direction);
+
+
+    public List<Node> GetNeighbors() => Neighbors.Select(k => k.To).ToList();
 }
 
-public static class GlobalExtensions {
-    public static List<string> ToStringList(this IEnumerable<Input> list) {
+public class Connection
+{
+    public Connection(Node from, Node to, EDirection dir)
+    {
+        From = from;
+        To = to;
+        Direction = dir;
+    }
+
+    public Node From { get; set; }
+    public Node To { get; set; }
+    public EDirection Direction { get; set; }
+}
+
+public enum EDirection
+{
+    N,
+    S,
+    E,
+    W,
+    NE,
+    NW,
+    SE,
+    SW
+}
+
+public static class EDirectionExtensions
+{
+    public static EDirection GetOppositeDirection(this EDirection direction) =>
+        direction switch
+        {
+            EDirection.N => EDirection.S,
+            EDirection.S => EDirection.N,
+            EDirection.E => EDirection.W,
+            EDirection.W => EDirection.E,
+            EDirection.NE => EDirection.SW,
+            EDirection.NW => EDirection.SE,
+            EDirection.SE => EDirection.NW,
+            EDirection.SW => EDirection.NE,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+}
+
+public static class GlobalExtensions
+{
+    public static List<string> ToStringList(this IEnumerable<Input> list)
+    {
         return list.Select(k => String.Join(String.Empty, k.Inputs)).ToList();
     }
 
-    public static List<int> ToIntList(this IEnumerable<string> list) {
+    public static List<int> ToIntList(this IEnumerable<string> list)
+    {
         return list.Select(k => k.ToInt32()).ToList();
     }
 
-    public static List<List<int>> ToIntList(this IEnumerable<Input> list) {
+    public static List<List<int>> ToIntList(this IEnumerable<Input> list)
+    {
         return list.Select(k => k.Inputs.ToIntList()).ToList();
     }
 
-    public static List<List<string>> ToStringsList(this IEnumerable<Input> list) {
+    public static List<List<string>> ToStringsList(this IEnumerable<Input> list)
+    {
         return list.Select(k => k.Inputs).ToList();
     }
 
-    public static List<List<T>> ToBoxList<T>(this IEnumerable<T> list, T Compare) {
+    public static List<List<T>> ToBoxList<T>(this IEnumerable<T> list, T Compare)
+    {
         var boxed = new List<List<T>>();
-        foreach (var item in list) {
-            if (item.Equals(Compare)) {
+        foreach (var item in list)
+        {
+            if (item.Equals(Compare))
+            {
                 boxed.Add(new List<T>());
             }
-            else {
+            else
+            {
                 if (boxed.Count == 0) continue;
                 boxed[^1].Add(item);
             }
@@ -316,12 +433,15 @@ public static class GlobalExtensions {
         return boxed;
     }
 
-    public static List<List<T>> Cluster<T>(this IEnumerable<T> list, int count) {
+    public static List<List<T>> Cluster<T>(this IEnumerable<T> list, int count)
+    {
         List<List<T>> boxed = new List<List<T>>();
         int i = 0;
 
-        foreach (var item in list) {
-            if (i % count == 0) {
+        foreach (var item in list)
+        {
+            if (i % count == 0)
+            {
                 boxed.Add(new List<T>());
             }
 
@@ -332,8 +452,10 @@ public static class GlobalExtensions {
         return boxed;
     }
 
-    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> list, Action<T> action) {
-        foreach (var item in list) {
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> list, Action<T> action)
+    {
+        foreach (var item in list)
+        {
             action(item);
         }
 
---
+++ D08_Trees/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using AdventOfCode2022;
+
+var inputs = InputReader.CreateInputReader().ReadFilePerLine("input2.txt").GetInputs().ToStringList()
+    .Select(k => k.Select(c => new Tree() { Height = Convert.ToInt32(c.ToString()) }).ToList()).ToList();
+
+var nodes = inputs.ToNeighbors().ToType<Tree>();
+
+var edgeTrees = nodes.Where(k => k.Neighbors.Count < 4).ToList();
+var innerTrees = nodes.Where(k => k.Neighbors.Count == 4).ToList();
+var levelDirection = new List<EDirection>() { EDirection.E, EDirection.W, EDirection.S, EDirection.N };
+
+void level1(List<Tree> trees, List<List<Tree>>? list)
+{
+    void PrintTrees()
+    {
+        int factor = 64;
+
+        StringFormat format = new StringFormat()
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center
+        };
+
+        //create an empty png and draw the trees on it
+        var bitmap = new Bitmap(list.Count * factor, list.First().Count * factor);
+        var graphics = Graphics.FromImage(bitmap);
+        graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+        graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+        graphics.Clear(Color.White);
+
+        var font = new Font("Arial", factor * 0.66f);
+        var x = 0;
+        var y = 0;
+        list.ForEach(k =>
+        {
+            k.ForEach(tree =>
+            {
+                var height = tree.Height;
+                if (tree.isVisible)
+                {
+                    graphics.FillRectangle(Brushes.Green, x, y, factor, factor);
+                }
+                else
+                {
+                    graphics.FillRectangle(Brushes.Red, x, y, factor, factor);
+                }
+
+                graphics.DrawString(height.ToString(), font, Brushes.Black, new RectangleF(x, y, factor, factor),
+                    format);
+                x += factor;
+            });
+            x = 0;
+            y += factor;
+        });
+        graphics.Flush();
+        bitmap.Save("../../../trees.png");
+    }
+
+
+    foreach (var direction in levelDirection)
+    {
+        var nodesWithStartDirection =
+            trees.Where(k => k.Neighbors.All(n => n.Direction != direction.GetOppositeDirection())).ToList();
+
+        var queue = new Queue<QueueElement>();
+
+
+        nodesWithStartDirection.ForEach(k => queue.Enqueue(new(k, -1)));
+
+        while (queue.Any())
+        {
+            var (node, heightOfLastVisibleNode) = queue.Dequeue();
+            var neighbor = node.GetNeighbor(direction);
+            if (neighbor is null) continue;
+
+            var nextNode = neighbor.To as Tree;
+
+            if (nextNode is null) continue;
+
+
+            if (node.Height > heightOfLastVisibleNode)
+            {
+                node.isVisible = true;
+                heightOfLastVisibleNode = node.Height;
+            }
+
+
+            queue.Enqueue(new(nextNode, heightOfLastVisibleNode));
+        }
+
+        PrintTrees();
+    }
+
+    Console.WriteLine(nodes.Count(k => k.isVisible));
+}
+
+void Level2()
+{
+    var x = nodes.Select(k => levelDirection.Select(d => GetNumberOfAdjacentTreesSmallerThanThis(d, k, k.Height)));
+    var maxNumber = x.Select(k => k.Aggregate((x, i) => (x > 0 ? x : 1) * i));
+    Console.WriteLine(maxNumber.Max());
+
+    int GetNumberOfAdjacentTreesSmallerThanThis(EDirection direction, Tree tree, int height)
+    {
+        var node = tree.GetNeighbor(direction);
+        if (node is null) return 0;
+        var nextTree = node.To as Tree;
+        if (nextTree.Height >= height) return 1;
+        return 1 + GetNumberOfAdjacentTreesSmallerThanThis(direction, nextTree, height);
+    }
+}
+
+Level2();
+
+
+record QueueElement(Tree tree, int heightOfLastVisibleNode);
+
+class Tree : Node
+{
+    public bool isVisible { get; set; } = false;
+    public int Height { get; set; }
+}
