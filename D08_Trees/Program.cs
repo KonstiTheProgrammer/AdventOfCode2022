e22a7772b515c07a450e8874dd42e3372c8830eb
konstitheprogrammer <kanzlerkonstantin@gmail.com>
2022-12-10 15:04:35 +0100

    added day 9 and 10

--- D08_Trees/Program.cs
+++ D08_Trees/Program.cs
@@ -3,22 +3,19 @@ using System.Drawing;
 using AdventOfCode2022;
 
 var inputs = InputReader.CreateInputReader().ReadFilePerLine("input2.txt").GetInputs().ToStringList()
-    .Select(k => k.Select(c => new Tree() { Height = Convert.ToInt32(c.ToString()) }).ToList()).ToList();
+    .Select(k => k.Select(c => new Tree(){ Height = Convert.ToInt32(c.ToString()) }).ToList()).ToList();
 
 var nodes = inputs.ToNeighbors().ToType<Tree>();
 
 var edgeTrees = nodes.Where(k => k.Neighbors.Count < 4).ToList();
 var innerTrees = nodes.Where(k => k.Neighbors.Count == 4).ToList();
-var levelDirection = new List<EDirection>() { EDirection.E, EDirection.W, EDirection.S, EDirection.N };
+var levelDirection = new List<EDirection>(){ EDirection.E, EDirection.W, EDirection.S, EDirection.N };
 
-void level1(List<Tree> trees, List<List<Tree>>? list)
-{
-    void PrintTrees()
-    {
+void level1(List<Tree> trees, List<List<Tree>>? list){
+    void PrintTrees(){
         int factor = 64;
 
-        StringFormat format = new StringFormat()
-        {
+        StringFormat format = new StringFormat(){
             Alignment = StringAlignment.Center,
             LineAlignment = StringAlignment.Center
         };
@@ -34,17 +31,13 @@ void level1(List<Tree> trees, List<List<Tree>>? list)
         var font = new Font("Arial", factor * 0.66f);
         var x = 0;
         var y = 0;
-        list.ForEach(k =>
-        {
-            k.ForEach(tree =>
-            {
+        list.ForEach(k => {
+            k.ForEach(tree => {
                 var height = tree.Height;
-                if (tree.isVisible)
-                {
+                if (tree.isVisible){
                     graphics.FillRectangle(Brushes.Green, x, y, factor, factor);
                 }
-                else
-                {
+                else{
                     graphics.FillRectangle(Brushes.Red, x, y, factor, factor);
                 }
 
@@ -60,8 +53,7 @@ void level1(List<Tree> trees, List<List<Tree>>? list)
     }
 
 
-    foreach (var direction in levelDirection)
-    {
+    foreach (var direction in levelDirection){
         var nodesWithStartDirection =
             trees.Where(k => k.Neighbors.All(n => n.Direction != direction.GetOppositeDirection())).ToList();
 
@@ -70,8 +62,7 @@ void level1(List<Tree> trees, List<List<Tree>>? list)
 
         nodesWithStartDirection.ForEach(k => queue.Enqueue(new(k, -1)));
 
-        while (queue.Any())
-        {
+        while (queue.Any()){
             var (node, heightOfLastVisibleNode) = queue.Dequeue();
             var neighbor = node.GetNeighbor(direction);
             if (neighbor is null) continue;
@@ -81,8 +72,7 @@ void level1(List<Tree> trees, List<List<Tree>>? list)
             if (nextNode is null) continue;
 
 
-            if (node.Height > heightOfLastVisibleNode)
-            {
+            if (node.Height > heightOfLastVisibleNode){
                 node.isVisible = true;
                 heightOfLastVisibleNode = node.Height;
             }
@@ -97,14 +87,12 @@ void level1(List<Tree> trees, List<List<Tree>>? list)
     Console.WriteLine(nodes.Count(k => k.isVisible));
 }
 
-void Level2()
-{
+void Level2(){
     var x = nodes.Select(k => levelDirection.Select(d => GetNumberOfAdjacentTreesSmallerThanThis(d, k, k.Height)));
     var maxNumber = x.Select(k => k.Aggregate((x, i) => (x > 0 ? x : 1) * i));
     Console.WriteLine(maxNumber.Max());
 
-    int GetNumberOfAdjacentTreesSmallerThanThis(EDirection direction, Tree tree, int height)
-    {
+    int GetNumberOfAdjacentTreesSmallerThanThis(EDirection direction, Tree tree, int height){
         var node = tree.GetNeighbor(direction);
         if (node is null) return 0;
         var nextTree = node.To as Tree;
@@ -118,8 +106,7 @@ Level2();
 
 record QueueElement(Tree tree, int heightOfLastVisibleNode);
 
-class Tree : Node
-{
-    public bool isVisible { get; set; } = false;
-    public int Height { get; set; }
+class Tree : Node{
+    public bool isVisible{ get; set; } = false;
+    public int Height{ get; set; }
 }
